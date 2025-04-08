0f6950381e52030586d91c3d100d7a469c96ef15
Aria0525 <6887774@gmail.com>
2025-04-08 14:59:22 +0800

    AliYunSignin.js

--- AliYunSignin.js
+++ AliYunSignin.js
@@ -25,8 +25,8 @@ BoxJs订阅地址:
 https://raw.githubusercontent.com/leiyiyan/resource/main/subscribe/leiyiyan.boxjs.json
 
 [Script]
-http-response ^https?:\/\/developer\.aliyun\.com\/developer\/api\/my\/user\/getUser script-path=https://raw.githubusercontent.com/Aria0525/AliYun/refs/heads/main/AliYunSignin.js, requires-body=true, timeout=60, tag=阿里云Web Cookie
-cron "0 7,13 * * *" script-path=https://raw.githubusercontent.com/Aria0525/AliYun/refs/heads/main/AliYunSignin.js, tag=阿里云社区日常任务
+http-response ^https?:\/\/developer\.aliyun\.com\/developer\/api\/my\/user\/getUser script-path=https://raw.githubusercontent.com/leiyiyan/resource/main/script/aliyun_web/aliyun_web.js, requires-body=true, timeout=60, tag=阿里云Web Cookie
+cron "0 7,13 * * *" script-path=https://raw.githubusercontent.com/leiyiyan/resource/main/script/aliyun_web/aliyun_web.js, tag=阿里云社区日常任务
 
 [MITM]
 hostname = developer.aliyun.com
